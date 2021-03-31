a46740cdeff1c1451236d4f7490f8ccc85770509
lianggs233 <852601514@qq.com>
2021-03-31 20:13:18 +0800

    'identifycode'

--- demo/src/components/Identify.vue
+++ demo/src/components/Identify.vue
@@ -45,7 +45,7 @@ export default {
       type: Number,
       default: 90
     },
-    contentHeight:{
+    contentHeight: {
       /* 容器高度 */
       type: Number,
       default: 38
@@ -70,13 +70,62 @@ export default {
 
     drawPic () {
       let canvas = document.getElementById('s-canvas')
-      let ctx = canvas.getContext('2d')
+      let ctx = canvas.getContext('2d') /* 指定了二维绘图，并且导致这个方法返回一个环境对象，该对象导出一个二维绘图 API。 */
       ctx.textBaseline = 'bottom'
       /* 绘制背景 */
       ctx.fillStyle = '#e6ecfd'
       ctx.fillRect(0, 0, this.contentWidth, this.contentHeight)
       /* 绘制文字 */
+      for (let i = 0; i < this.identifyCode.length; i++) {
+        this.drawText(ctx, this.identifyCode[i], i)
+      }
+      this.drawLine(ctx)
+      this.drawDot(ctx)
+    },
+
+    drawText (ctx, txt, i) {
+      ctx.fillStyle = this.randomColor(50, 160)/* 随机生成字体颜色 */
+      ctx.font = this.randomNum(this.fontSizeMin, this.fontSizeMax) + 'px SimHei' /* 随机生成字体大小 */
+      let x = (i + 1) * (this.contentWidth / (this.identifyCode + 1))
+      let y = this.randomNum(this.fontSizeMax, this.contentHeight - 5)
+      var deg = this.randomNum(-30, 30)
+      /* 修改坐标原点和旋转角度 */
+      ctx.translate(x, y)
+      ctx.rotate(deg * Math.PI / 180)
+      ctx.fillText(txt, 0, 0)
+      /* 恢复坐标原点和旋转角度 */
+      ctx.rotate(-deg * Math.PI / 180)
+      ctx.translate(-x, -y)
+    },
+
+    drawLine (ctx) {
+      /* 绘制干扰线 */
+      for (let i = 0; i < 4; i++) {
+        ctx.strokeStyle = this.randomColor(100, 200)
+        ctx.beginPath()
+        ctx.moveTo(this.randomNum(0, this.contentWidth), this.randomNum(0, this.contentHeight))
+        ctx.lineTo(this.randomNum(0, this.contentWidth), this.randomNum(0, this.contentHeight))
+        ctx.stroke()
+      }
+    },
+
+    drawDot (ctx) {
+    /* 绘制干扰点 */
+      for (let i = 0; i < 30; i++) {
+        ctx.fillStyle = this.randomColor(0, 255)
+        ctx.beginPath()
+        ctx.arc(this.randomNum(0, this.contentWidth), this.randomNum(0, this.contentHeight), 1, 0, 2 * Math.PI)
+        ctx.fill()
+      }
     }
   },
+  watch: {
+    identifyCode () {
+      this.drawPic()
+    }
+  },
+  mounted () {
+    this.drawPic()
+  }
 }
 </script>
